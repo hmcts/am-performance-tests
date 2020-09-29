8e0aa69b0e1979d6f22531a4ae0b7c1d240815d5
Amogh Naik <amoghvnaik@amoghs-mbp.home>
2020-09-29 15:54:17 +0100

    added perftest env

--- src/gatling/simulations/uk/gov/hmcts/reform/role_assignment/performance/scenarios/utils/Environment.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/role_assignment/performance/scenarios/utils/Environment.scala
@@ -4,31 +4,32 @@ import io.gatling.http.Predef._
 
 object Environment {
 
-  val baseURL = "http://am-role-assignment-service-aat.service.core-compute-aat.internal"
+  //val baseURL = "http://am-role-assignment-service-aat.service.core-compute-aat.internal"
+  val baseURL = "https://idam-api.perftest.platform.hmcts.net"
 
- val headers_1 = Map(
+  val headers_1 = Map(
    "Authorization" -> "Bearer ${accessToken}",
    "serviceAuthorization" -> "${s2sToken}"
- )
+  )
 
- val headers_2 = Map(
+  val headers_2 = Map(
   "actorId" -> "${actorId}"
- )
+  )
 
- val headers_3 = Map(
+  val headers_3 = Map(
   "roleType" -> "CASE"
- )
+  )
 
- val headers_4 = Map(
+  val headers_4 = Map(
   "content-type" -> "application/json"
- )
+  )
 
- val headers_5 = Map(
+  val headers_5 = Map(
   "assignmentId" -> "${assignmentId}"
- )
+  )
 
- val feederFile = csv("Feeder_file.csv").random
+  val feederFile = csv("Feeder_file.csv").random
 
- val thinkTime = 10
+  val thinkTime = 10
 
 }
