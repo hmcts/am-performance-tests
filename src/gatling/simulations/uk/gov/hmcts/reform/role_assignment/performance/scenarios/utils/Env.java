86e3b37a7c331d7f6dd70a712ebb9152faa3f5e0
amoghnaik-hmcts <amogh.naik@hmcts.net>
2020-10-01 14:59:34 +0100

    changed pertest env user

--- src/gatling/simulations/uk/gov/hmcts/reform/role_assignment/performance/scenarios/utils/Env.java
+++ src/gatling/simulations/uk/gov/hmcts/reform/role_assignment/performance/scenarios/utils/Env.java
@@ -15,9 +15,9 @@ public class Env {
         defaults.setProperty("IDAM_OAUTH_SECRET", ConfigFactory.load().getString("IDAM_SECRET_PERFTEST"));
         defaults.setProperty("IDAM_SCOPE", "openid%20profile%20roles%20authorities");
         //defaults.setProperty("IDAM_USERNAME", "befta.caseworker.2.solicitor.2@gmail.com");
-        defaults.setProperty("IDAM_USERNAME", "ccdloadtest1@gmail.com");
+        defaults.setProperty("IDAM_USERNAME", "amogh.role.assignments@mailinator.com");
         //defaults.setProperty("IDAM_PASSWORD", "PesZvqrb78");
-        defaults.setProperty("IDAM_PASSWORD", "Password12");
+        defaults.setProperty("IDAM_PASSWORD", "Pass19word");
         //defaults.setProperty("S2S_BASE_URI", "http://rpe-service-auth-provider-aat.service.core-compute-aat.internal/testing-support");
         defaults.setProperty("S2S_BASE_URI", "http://rpe-service-auth-provider-perftest.service.core-compute-perftest.internal/testing-support");
         defaults.setProperty("FUNCTIONAL_TEST_CLIENT_S2S_TOKEN", ConfigFactory.load().getString("S2S_SECRET"));
