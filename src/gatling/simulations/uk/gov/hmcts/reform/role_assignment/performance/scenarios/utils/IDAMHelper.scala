8afeddf074fb8346e510027e93a057f0aaedf3c0
amoghnaik-hmcts <70205551+amoghnaik-hmcts@users.noreply.github.com>
2020-10-05 10:41:30 +0100

    Update IDAMHelper.scala

--- src/gatling/simulations/uk/gov/hmcts/reform/role_assignment/performance/scenarios/utils/IDAMHelper.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/role_assignment/performance/scenarios/utils/IDAMHelper.scala
@@ -13,7 +13,7 @@ object IDAMHelper {
          .post(Env.getIdamUrl()+"/o/token?client_id="+Env.getOAuthClient()+"&client_secret="+Env.getOAuthSecret()+"&grant_type=password&scope="+Env.getScope()+"&username="+Env.getUsername()+"&password="+Env.getPassword())
          .header("Content-Type", "application/x-www-form-urlencoded")
          .header("Content-Length", "0")
-         .check(status is 200)
+         .check(status.is(200))
          .check(jsonPath("$.access_token").saveAs("accessToken")))
     .pause(2)
 
