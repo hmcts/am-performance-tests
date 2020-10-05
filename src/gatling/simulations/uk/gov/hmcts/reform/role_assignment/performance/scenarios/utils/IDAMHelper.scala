64d97c2615b1fcb76f2fafbc9988ff33ee83dd69
Amogh Naik <amoghvnaik@amoghs-mbp.home>
2020-10-05 12:17:36 +0100

    changed build.gradle

--- src/gatling/simulations/uk/gov/hmcts/reform/role_assignment/performance/scenarios/utils/IDAMHelper.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/role_assignment/performance/scenarios/utils/IDAMHelper.scala
@@ -12,9 +12,9 @@ object IDAMHelper {
     exec(http("PaymentAPI_010_015_GetAuthToken")
          .post(Env.getIdamUrl()+"/o/token?client_id="+Env.getOAuthClient()+"&client_secret="+Env.getOAuthSecret()+"&grant_type=password&scope="+Env.getScope()+"&username="+Env.getUsername()+"&password="+Env.getPassword())
          .header("Content-Type", "application/x-www-form-urlencoded")
-         .header("Content-Length", "0"))
-         //.check(status.is(200))
-         //.check(jsonPath("$.access_token").saveAs("accessToken")))
+         .header("Content-Length", "0")
+         .check(status.is(200))
+         .check(jsonPath("$.access_token").saveAs("accessToken")))
     .pause(2)
 
 
