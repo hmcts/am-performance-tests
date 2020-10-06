9fb3c9db27f206257437d0ec49f771d35b4ed15d
amoghnaik-hmcts <70205551+amoghnaik-hmcts@users.noreply.github.com>
2020-10-06 17:05:31 +0100

    Update Scenario2.scala

--- src/gatling/simulations/uk/gov/hmcts/reform/role_assignment/performance/scenarios/Scenario2.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/role_assignment/performance/scenarios/Scenario2.scala
@@ -16,30 +16,16 @@ object Scenario2 {
       .headers(Environment.headers_4)
       .body(ElFileBody("body.json"))
       .check(status.is(201))
-      .check(bodyString.saveAs("BODY1"))
       .check(jsonPath("$..actorId").saveAs("actorId"))
       .check(jsonPath("$..caseId").saveAs("caseId"))
       .check(jsonPath("$..id").saveAs("assignmentId"))
       .check(jsonPath("$..process").saveAs("process"))
       .check(jsonPath("$..reference").saveAs("reference")))
-    .exec{
-      session =>
-        println("This is the response body of AM_010_PostRoleAssignments:" + session("BODY1").as[String])
-        println("This is the IDAM token:" + session("accessToken").as[String])
-        println("This is the S2S token:" + session("s2sToken").as[String])
-        session
-    }
     .pause(2)
 
     .exec(http(requestName="AM_070_DeleteRoleAssignmentsReference")
       .delete("/am/role-assignments?process=${process}&reference=${reference}")
       .headers(Environment.headers_1)
-      .check(status.is(204))
-      .check(bodyString.saveAs("BODY7")))
-    .exec{
-      session =>
-        println("This is the response body of AM_060_DeleteRoleAssignmentsReference:" + session("BODY7").as[String])
-        session
-    }
+      .check(status.is(204)))
     .pause(2)
 }
