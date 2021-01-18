fb391ab227a210da192336f93fc7632798851036
amoghnaik-hmcts <70205551+amoghnaik-hmcts@users.noreply.github.com>
2021-01-18 14:20:50 +0000

    Update RoleAssignmentSimulation.scala

--- src/gatling/simulations/uk/gov/hmcts/reform/role_assignment/performance/simulations/RoleAssignmentSimulation.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/role_assignment/performance/simulations/RoleAssignmentSimulation.scala
@@ -16,7 +16,7 @@ class RoleAssignmentSimulation extends Simulation{
 
         .exec(IDAMHelper.getIdamToken)
         .exec(S2SHelper.S2SAuthToken)
-    .repeat(10)
+    .repeat(1)
     {
       feed(feederFile)
         .exec(Scenario1.Scenario1)
