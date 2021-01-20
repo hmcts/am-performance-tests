508e6c2d4c82d65bfa2fd2d526d65bf037d0687f
amoghnaik-hmcts <70205551+amoghnaik-hmcts@users.noreply.github.com>
2021-01-20 16:50:56 +0000

    Update Scenario1.scala

--- src/gatling/simulations/uk/gov/hmcts/reform/role_assignment/performance/scenarios/Scenario1.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/role_assignment/performance/scenarios/Scenario1.scala
@@ -30,7 +30,10 @@ object Scenario1 {
       .body(ElFileBody("body.json"))
       .check(status.is(201))
       .check(jsonPath("$..actorId").saveAs("actorId"))
-      .check(jsonPath("$..id").saveAs("assignmentId")))
+      .check(jsonPath("$..id").saveAs("assignmentId"))
+      .check(jsonPath("$..caseId").saveAs("caseId"))
+      .check(jsonPath("$..process").saveAs("process"))
+      .check(jsonPath("$..reference").saveAs("reference")))
     .pause(5)
 
     // posts role assignments from body3.json
@@ -43,24 +46,45 @@ object Scenario1 {
       .check(jsonPath("$..process").saveAs("process1"))
       .check(jsonPath("$..reference").saveAs("reference1")))
     .pause(5)
+    
+    // posts role assignments from body4.json
+    .pause(10)
+    .exec(http(requestName="AM_030_PostRoleAssignments")
+      .post("/am/role-assignments")
+      .headers(Environment.headers_1)
+      .headers(Environment.headers_4)
+      .body(ElFileBody("body4.json"))
+      .check(status.is(201))
+      .check(jsonPath("$..process").saveAs("process2"))
+      .check(jsonPath("$..reference").saveAs("reference2")))
+    .pause(5)
 
     // gets roles
-    .exec(http(requestName="AM_030_GetRoles")
+    .exec(http(requestName="AM_040_GetRoles")
       .get("/am/role-assignments/roles")
       .headers(Environment.headers_1)
       .check(status.is(200)))
     .pause(5)
 
     // gets role assignments by actor
-    .exec(http(requestName="AM_040_GetRoleAssignmentsActor")
+    .exec(http(requestName="AM_050_GetRoleAssignmentsActor")
       .get("/am/role-assignments/actors/${actorId}")
       .headers(Environment.headers_1)
       .headers(Environment.headers_2)
       .check(status.is(200)))
     .pause(5)
+    
+    // queries role assignments
+    .exec(http(requestName="AM_060_QueryRoleAssignments")
+      .post("/am/role-assignments/query")
+      .headers(Environment.headers_1)
+      .headers(Environment.headers_4)
+      .body(ElFileBody("body2.json"))
+      .check(status.is(200)))
+    .pause(5)
 
     // deletes role assignments
-    .exec(http(requestName="AM_050_DeleteRoleAssignments")
+    .exec(http(requestName="AM_070_DeleteRoleAssignments")
       .delete("/am/role-assignments/${assignmentId}")
       .headers(Environment.headers_1)
       .headers(Environment.headers_5)
@@ -68,8 +92,15 @@ object Scenario1 {
     .pause(5)
 
     // deletes role assignments by process and reference
-    .exec(http(requestName="AM_060_DeleteRoleAssignmentsReference")
+    .exec(http(requestName="AM_080_DeleteRoleAssignmentsReference")
       .delete("/am/role-assignments?process=${process1}&reference=${reference1}")
+      .headers(Environment.headers_1)
+      .check(status.is(204)))
+    .pause(5)
+    
+    // deletes role assignments by process and reference
+    .exec(http(requestName="AM_080_DeleteRoleAssignmentsReference")
+      .delete("/am/role-assignments?process=${process2}&reference=${reference2}")
       .headers(Environment.headers_1)
       .check(status.is(204)))
     .pause(5)
