8b86a9522ec9831376fc6fdcf3836fe75fd24d26
Nitin <nitin.sahu@hmcts.net>
2021-02-25 13:37:57 +0000

    Updated the assigner id as per the user

--- src/gatling/simulations/uk/gov/hmcts/reform/role_assignment/performance/scenarios/Scenario1.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/role_assignment/performance/scenarios/Scenario1.scala
@@ -34,7 +34,6 @@ object Scenario1 {
       .check(jsonPath("$..caseId").saveAs("caseId"))
       .check(jsonPath("$..process").saveAs("process"))
       .check(jsonPath("$..reference").saveAs("reference")))
-    .pause(10)
 
     // posts role assignments from body3.json
     .exec(http(requestName="AM_020_PostRoleAssignments")
@@ -45,7 +44,6 @@ object Scenario1 {
       .check(status.is(201))
       .check(jsonPath("$..process").saveAs("process1"))
       .check(jsonPath("$..reference").saveAs("reference1")))
-    .pause(10)
 
     // posts role assignments from body4.json
     .exec(http(requestName="AM_030_PostRoleAssignments")
@@ -56,52 +54,5 @@ object Scenario1 {
       .check(status.is(201))
       .check(jsonPath("$..process").saveAs("process2"))
       .check(jsonPath("$..reference").saveAs("reference2")))
-    .pause(10)
-
-    // gets roles
-    .exec(http(requestName="AM_040_GetRoles")
-      .get("/am/role-assignments/roles")
-      .headers(Environment.headers_1)
-      .check(status.is(200)))
-    .pause(10)
-
-    // gets role assignments by actor
-    .exec(http(requestName="AM_050_GetRoleAssignmentsActor")
-      .get("/am/role-assignments/actors/${actorId}")
-      .headers(Environment.headers_1)
-      .headers(Environment.headers_2)
-      .check(status.is(200)))
-    .pause(10)
-
-    // queries role assignments
-    .exec(http(requestName="AM_060_QueryRoleAssignments")
-      .post("/am/role-assignments/query")
-      .headers(Environment.headers_1)
-      .headers(Environment.headers_4)
-      .body(ElFileBody("body2.json"))
-      .check(status.is(200)))
-    .pause(10)
-
-    // deletes role assignments
-    .exec(http(requestName="AM_070_DeleteRoleAssignments")
-      .delete("/am/role-assignments/${assignmentId}")
-      .headers(Environment.headers_1)
-      .headers(Environment.headers_5)
-      .check(status.is(204)))
-    .pause(10)
-
-    // deletes role assignments by process and reference
-    .exec(http(requestName="AM_080_DeleteRoleAssignmentsReference")
-      .delete("/am/role-assignments?process=${process1}&reference=${reference1}")
-      .headers(Environment.headers_1)
-      .check(status.is(204)))
-    .pause(10)
-
-    // deletes role assignments by process and reference
-    .exec(http(requestName="AM_090_DeleteRoleAssignmentsReference")
-      .delete("/am/role-assignments?process=${process2}&reference=${reference2}")
-      .headers(Environment.headers_1)
-      .check(status.is(204)))
-    .pause(10)
 
 }
