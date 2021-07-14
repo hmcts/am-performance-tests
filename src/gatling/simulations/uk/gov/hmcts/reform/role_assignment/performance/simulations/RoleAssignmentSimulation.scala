c06e13b21609ba77b0875ebfd1df6b8a5acc3c9b
jmcadam <mcadam88@gmail.com>
2021-07-14 14:51:49 +0100

    Update RoleAssignmentSimulation.scala

--- src/gatling/simulations/uk/gov/hmcts/reform/role_assignment/performance/simulations/RoleAssignmentSimulation.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/role_assignment/performance/simulations/RoleAssignmentSimulation.scala
@@ -26,7 +26,7 @@ class RoleAssignmentSimulation extends Simulation{
 
   // val getRoleAssignmentsByActorPeakTarget:Double = 350
   // val getRoleAssignmentsByActorRate: Double = getRoleAssignmentsByActorPeakTarget / 6
-  val getRoleAssignmentsByActorRate: Double = 60
+  val getRoleAssignmentsByActorRate: Double = 90
 
   val queryRoleAssignmentsPeakTarget:Double = 20
   val queryRoleAssignmentsRate: Double = queryRoleAssignmentsPeakTarget / 60
